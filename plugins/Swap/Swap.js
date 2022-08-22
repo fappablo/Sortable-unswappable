3e5f1da6d799733462e1a21183dd92d3f5fdcb8a
Fappablo <thefappachannelita@gmail.com>
2022-08-22 17:17:12 +0200

    test

--- plugins/Swap/Swap.js
+++ plugins/Swap/Swap.js
@@ -71,20 +71,20 @@ function SwapPlugin() {
 
 
 function swapNodes(n1, n2) {
-	// let p1 = n1.parentNode,
-	// 	p2 = n2.parentNode,
-	// 	i1, i2;
+	let p1 = n1.parentNode,
+		p2 = n2.parentNode,
+		i1, i2;
 
-	// if (!p1 || !p2 || p1.isEqualNode(n2) || p2.isEqualNode(n1)) return;
+	if (!p1 || !p2 || p1.isEqualNode(n2) || p2.isEqualNode(n1)) return;
 
-	// i1 = index(n1);
-	// i2 = index(n2);
+	i1 = index(n1);
+	i2 = index(n2);
 
-	// if (p1.isEqualNode(p2) && i1 < i2) {
-	// 	i2++;
-	// }
-	// p1.insertBefore(n2, p1.children[i1]);
-	// p2.insertBefore(n1, p2.children[i2]);
+	if (p1.isEqualNode(p2) && i1 < i2) {
+		i2++;
+	}
+	p1.insertBefore(n2, p1.children[i1]);
+	p2.insertBefore(n1, p2.children[i2]);
 }
 
 export default SwapPlugin;
