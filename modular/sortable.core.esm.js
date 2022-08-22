07c1d1cfba77c54e5381184f1b6581332cd9403a
Fappablo <thefappachannelita@gmail.com>
2022-08-22 17:18:08 +0200

    test

--- modular/sortable.core.esm.js
+++ modular/sortable.core.esm.js
@@ -3103,6 +3103,7 @@ function SwapPlugin() {
         if (dragEl !== lastSwapEl) {
           toSortable.captureAnimationState();
           if (toSortable !== activeSortable) activeSortable.captureAnimationState();
+          swapNodes(dragEl, lastSwapEl);
           toSortable.animateAll();
           if (toSortable !== activeSortable) activeSortable.animateAll();
         }
@@ -3120,6 +3121,23 @@ function SwapPlugin() {
       };
     }
   });
+}
+
+function swapNodes(n1, n2) {
+  var p1 = n1.parentNode,
+      p2 = n2.parentNode,
+      i1,
+      i2;
+  if (!p1 || !p2 || p1.isEqualNode(n2) || p2.isEqualNode(n1)) return;
+  i1 = index(n1);
+  i2 = index(n2);
+
+  if (p1.isEqualNode(p2) && i1 < i2) {
+    i2++;
+  }
+
+  p1.insertBefore(n2, p1.children[i1]);
+  p2.insertBefore(n1, p2.children[i2]);
 }
 
 var multiDragElements = [],
